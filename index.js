03038f882571917979648b83c7d92f46769e7c95
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-08-20 11:40:30 +0200

    revert testing pug

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
+require('dotenv').config()
+require('./db/connection')
+
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
-require('./db/connection')
+const port = process.env.PORT
 
 const userRouter = require('./router/userRouter')
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(userRouter)
-
-app.get('/', (req, res) => {
-  res.status(200).render('base')
-})
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
