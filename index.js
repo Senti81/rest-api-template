8ed555b0d586f9ab917e47fd200cdef41a8ce9fc
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-08-24 15:30:02 +0200

    added cors middleware

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ require('./db/connection')
 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT
 
 const userRouter = require('./router/userRouter')
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 
