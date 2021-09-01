bfed55de9aabe1702e8b0b93e5391ac21c312564
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2021-09-01 17:08:54 +0200

    added model and route for Todo

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ const port = process.env.PORT
 
 const userRouter = require('./router/userRouter')
 const taskRouter = require('./router/taskRouter')
+const todoRouter = require('./router/todoRouter')
 
 app.use(cors())
 app.use(express.json())
+
 app.use(userRouter)
 app.use(taskRouter)
+app.use(todoRouter)
 
 app.listen(port, () => {
   console.log('Server is running on port ' + port)
---
+++ router/todoRouter.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const Todo = require('../model/Todo')
+const router = express.Router()
+
+router.post('/todos', async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title
+  })
+  try {
+    await todo.save()
+    res.status(201).send(todo)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+router.get('/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find()
+    res.send(todos)
+  } catch (error) {
+    res.status(400).send(error)
+  }
+})
+
+router.put('/todos/:id', async (req, res) => {
+  const _id =  req.params.id
+  try {
+    const todo = await Todo.findByIdAndUpdate(_id, { completed: req.body.completed })
+    if (!todo)
+      return res.status(404).send()
+    res.send()
+  } catch (error) {
+    res.status(500).send(error) 
+  }
+})
+
+router.delete('/todos/:id', async (req, res) => {
+  const _id =  req.params.id
+  try {
+    const todo = await Todo.findByIdAndRemove(_id)
+    if (!todo)
+      return res.status(400).send()
+    res.send(todo)
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
+module.exports = router
